47e02c948859bc3554d909c4a5aab520ba4e7ba2
reiory <reiory820402@gmail.com>
2014-03-10 02:31:21 +0800

    Create week3_3.java

---
+++ week3_3.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+public class week3_3
+{
+	public static void main(String arg[]) {
+		HashMap<String,Integer> hash = new HashMap<String,Integer>();
+		Scanner scanner = new Scanner(System.in);
+		String string;
+		int opt,sum=0,hold=0;
+		while(true)	{
+			System.out.print("請輸入選項：1)輸入新字串 2)查詢功能 -1)退出:");
+			opt=scanner.nextInt();
+			if(opt==-1)
+				break;
+			else if(opt==1){
+				System.out.print("請輸入字串：");
+				string = scanner.nextLine();
+				string = scanner.nextLine();
+				String[] tokens = string.split("[ |,|\\.|?|!]");
+				for(String S : tokens){
+					if(S.length() > 0){
+						if(isNumeric_Loop(S) == true) {
+							hold = Integer.parseInt(S);
+							sum = hold + sum;
+						}
+						else {
+							S=S.toLowerCase();
+							if(hash.get(S)== null)
+								hash.put(S,1);
+							else	{
+							int newValue = hash.get(S)+1;
+							hash.put(S,newValue);
+							}
+						}
+					}
+				}
+			}
+			else if(opt==2)	{
+				for(String key : hash.keySet()){
+					System.out.println(key+":"+hash.get(key));
+				}
+				System.out.println("sum="+sum);
+			}
+		}
+	}
+	public static boolean isNumeric_Loop(String str) {          
+		for (int i = str.length();--i>=0;){                 
+			if (!Character.isDigit(str.charAt(i))){                  
+				return false;              
+			}         
+		}          
+		return true;      
+	}
+}
